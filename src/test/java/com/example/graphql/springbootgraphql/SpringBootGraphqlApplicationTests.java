553e07a7e528e8294d7ad10edf16dd688815ce9c
Chetan Kokil <chetankokil@chetans-mbp.lan>
2020-04-21 23:33:25 -0500

    Modified to test case to pass the build

--- src/test/java/com/example/graphql/springbootgraphql/SpringBootGraphqlApplicationTests.java
+++ src/test/java/com/example/graphql/springbootgraphql/SpringBootGraphqlApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.graphql.springbootgraphql;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringBootGraphqlApplicationTests {
+public class SpringBootGraphqlApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
